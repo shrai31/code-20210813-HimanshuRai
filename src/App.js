11fd225a25228440366b9e955a8cf4703b05e7a6
shrai31 <shrai31@gmail.com>
2021-08-13 16:23:23 +0530

    "Count the overweight"

--- src/App.js
+++ src/App.js
@@ -1,111 +1,78 @@
-// import {useState} from 'react';
 import "./App.css";
-import GetCategory from './Category';
+
+export default function App() {
+  const data = [
+    { Gender: "Male", HeightCm: 171, WeightKg: 96 },
+    { Gender: "Male", HeightCm: 175, WeightKg: 85 },
+    { Gender: "Male", HeightCm: 180, WeightKg: 77 },
+    { Gender: "Female", HeightCm: 166, WeightKg: 62 },
+    { Gender: "Female", HeightCm: 150, WeightKg: 70 },
+    { Gender: "Female", HeightCm: 167, WeightKg: 82 },
+  ];
+
+    bmiCalculate()
+    getCategory()
+    getHealthRisk()
+     const count = `${ " Number of person overweight " + data.filter((val)=>val.cat ===  "Overweight" ).length }`; 
 
 
-const data = [
-  { Gender: "Male", HeightCm: 171, WeightKg: 96 },
-  { Gender: "Male", HeightCm: 175, WeightKg: 85 },
-  { Gender: "Male", HeightCm: 180, WeightKg: 77 },
-  { Gender: "Female", HeightCm: 166, WeightKg: 62 },
-  { Gender: "Female", HeightCm: 150, WeightKg: 70 },
-  { Gender: "Female", HeightCm: 167, WeightKg: 82 },
-];
+  function bmiCalculate() {
+    data.map((val)=>
+      val.bmivalue = (val.WeightKg / (val.HeightCm/100 * val.HeightCm/100)).toFixed(2)
+    )
+  }
 
- var BMI =0;
-
-function bmiCalculate (weight, height) {
-  BMI = (weight / (height * height)).toFixed(2);
-  // setBmi(BMI);
-  return BMI;
-}
-//  var obj = {category:"",healthRisk:""};
-const x = data.map((row , index) => {
-  return(
-  <tr key={index}>
-  <td>{row.Gender}</td>
-  <td>{row.HeightCm}</td>
-  <td>{row.WeightKg}</td>
-  <td>{bmiCalculate(row.WeightKg, row.HeightCm / 100)}</td>
-  <GetCategory bmi={BMI} />
-
-  {/* <td>
-  {console.log(BMI)}
-       {getCategory(BMI).category}
-       {console.log("mjhfgdfdfdghj",getCategory(20).category)}
-  </td>
-  <td>{getCategory(BMI).health}</td> */}
-</tr>)
-})
-
-
-
-
-// function getCategory(bmi) {
-//   // setBmi(100);
-//   // console.log("123243546578",bmi)
-//   switch(bmi){
-//     case bmi < 18.5:
-//       // obj.category=  "Underweight";
-//       // obj.healthRisk= "Malnutrition risk"
-//       return bmi ;
+  function getCategory() {
+    data.map((val)=>{
+      if (val.bmivalue < 18.5) {
+        val.cat =  "Underweight";
+      }
+      if (val.bmivalue >= 18.5 && val.bmivalue < 24.9) {
+        val.cat = "Normal weight";
+      }
+      if (val.bmivalue >= 25 && val.bmivalue < 29.9) {
+        val.cat =  "Overweight";
+      }
+      if (val.bmivalue >= 30 && val.bmivalue < 34.9) {
+        val.cat =  "Moderately obese";
+      }
+      if (val.bmivalue >= 35 && val.bmivalue < 39.9) {
+        val.cat =  "Severely obese";
+      }
+      if (val.bmivalue >= 40) {
+        val.cat = "Very severely obese";
+      } 
+      return val
+    })
+  }
+  function getHealthRisk() {
+    data.map(val=>{
+      if (val.bmivalue < 18.5) {
+        val.hr = "Malnutrition risk";
+      }
+      if (val.bmivalue>= 18.5 && val.bmivalue < 24.9) {
+        val.hr ="Low risk";
+      }
+      if (val.bmivalue >= 25 && val.bmivalue < 29.9) {
+        val.hr ="Enhanced risk";
+      }
+      if (val.bmivalue>= 30 && val.bmivalue < 34.9) {
+        val.hr = "Medium risk";
+      }
+      if (val.bmivalue>= 35 && val.bmivalue < 39.9) {
+        val.hr = "High risk";
+      }
+      if (val.bmivalue>= 40) {
+        val.hr = "Very high risk";
+      }
+      // if(val.cat =="Overweight"){
+      //   setcount(count+1)
+      // }
+      return val
+    })
+    console.log("",data)
+  }
   
-//     case bmi >= 18.5 && bmi < 24.9:
-//     // return "Normal weight";
-//     return {category:"Normal weight" , health:"Malnutrition risk"} ;
-
-  
-    // case bmi >= 25 && bmi < 29.9:
-    // // return "Overweight";
-    // return bmi ;
-
-  
-    // case bmi >= 30 && bmi < 34.9:
-    // // return "Moderately obese";
-    // return {category:"Moderately obese" , health:"Malnutrition risk"} ;
-
-  
-    // case bmi >= 35 && bmi < 39.9:
-    // // return "Severely obese";
-    // return {category:"Severely obese" , health:"Malnutrition risk"} ;
-
-  
-    // case bmi >= 40:
-    // // return "Very severely obese";
-    // return {category:"Very severely obese" , health:"Malnutrition risk"} ;
-
-
-//     default :
-//     return "";
-//   }
-  
-
-// }
-// console.log("function",getCategory(20));
-
-// function getHealthRisk(bmi) {
-//   if (bmi < 18.5) {
-//     return "Malnutrition risk";
-//   }
-//   if (bmi >= 18.5 && bmi < 24.9) {
-//     return "Low risk";
-//   }
-//   if (bmi >= 25 && bmi < 29.9) {
-//     return "Enhanced risk";
-//   }
-//   if (bmi >= 30 && bmi < 34.9) {
-//     return "Medium risk";
-//   }
-//   if (bmi >= 35 && bmi < 39.9) {
-//     return "High risk";
-//   }
-//   if (bmi >= 40) {
-//     return "Very high risk";
-//   }
-// }
-
-function Invoice() {
-  // const [bmi, setBmi] = useState(0);
   return (
     <div className="App">
       <table>
@@ -117,22 +84,18 @@ function Invoice() {
           <th>BMI Category</th>
           <th> Health risk</th>
         </tr>
-        {/* {data.map((row) => (
-          <tr>
+        {data.map((row,key) => (
+          <tr key={key}>
             <td>{row.Gender}</td>
             <td>{row.HeightCm}</td>
             <td>{row.WeightKg}</td>
-            <td>{bmiCalculate(row.WeightKg, row.HeightCm / 100)}</td>
-            <td>
-              {getCategory(bmiCalculate(row.WeightKg, row.HeightCm / 100))}
-            </td>
-            <td>{getHealthRisk(bmiCalculate(row.WeightKg, row.HeightCm / 100))}</td>
+            <td>{row.bmivalue}</td>
+            <td>{row.cat}</td>
+            <td>{row.hr}</td>
           </tr>
-        ))} */}
-        {x}
+        ))}
       </table>
+      {count}
     </div>
   );
 }
-
-export default Invoice;
